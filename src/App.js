b305720b4d788d65a1d9edf80eeab99a830c30fd
Karl Chvojka <karl.chvojka@protonmail.com>
2020-06-08 19:49:27 -0400

    Completed adding all the data

--- src/App.js
+++ src/App.js
@@ -17,13 +17,15 @@ function App() {
   const [user, setUser] = useState({});
 
   const [post, setPost] = useState({
-    postTitle: 'This is a post title',
+    postTitle: 'Developing JS applications with ReactJS',
+    postDesc: 'ReactJS is an amazing framework.Grapple shrouds stern crack Jennys tea cup Nelsons folly coxswain Sink me reef rigging tender. Broadside run a shot across the bow jack splice the main brace black spot bucko hardtack driver hands capstan.',
+    postUrl: 'https://karl.chvojka.com/blog/post-134',
     postAuth: "@KarlChvojka",
     postDate: "Oct 15",
     postImg: "/developer.jpeg",
     postComments: 2,
-    postRetweets: 20,
-    postLikes: 5000
+    postRetweets: 23,
+    postLikes: 345
   })
 
   useEffect(() => {
@@ -33,47 +35,52 @@ function App() {
   return (
     // check to see if the user obj has a first name
     Object.keys(user).length > 0 ?
-    <div className="App">
-      <header><h1>Social Card</h1></header>
-      <main>
-        <article>
-          <section className="social_Card_Wrap">
-            <aside className="image_Wrap">
-              <img src={user.picture.thumbnail} alt={`Thumbnail for ${user.name.first} ${user.name.last}`} />
-            </aside>
-            <article className="social_Card">
-              <header className="user_profile">
-                <p><b>The Dev Blog</b> @{user.login.username} &middot; {post.postDate}</p>
-                <p>{post.postTitle}</p>
-                <p>{'{'} author: {post.postAuth} {'}'}</p>
-              </header>
-              <section className="post_Image">
-                <img src={post.postImg} />
-              </section>
-              <section>
-                { /*
-                  TODO: Post title
-                  TODO: Text from the post itself
-                  TODO: Show text of the homepage from the link.
-                  */
-                }
-              </section>
-              <section>
-                { /*
-                  TODO: Number Representation of the comments
-                  TODO: Number Representation of the Retweets
-                  TODO: Number Representation of the Likes.
-                  TODO: Send via DM icon.
-                  */
-                }
-              </section>
-            </article>
-          </section>
-        </article>
-      </main>
-    </div>
+      <div className="App">
+        <header><h1>Social Card</h1></header>
+        <main>
+          <article>
+            <section className="social_Card_Wrap">
+              <aside className="image_Wrap">
+                <img src={user.picture.thumbnail} alt={`Thumbnail for ${user.name.first} ${user.name.last}`} />
+              </aside>
+              <article className="social_Card">
+                <header className="user_profile">
+                  <p><b>The Dev Blog</b> @{user.login.username} &middot; {post.postDate}</p>
+                  <p>{post.postTitle}</p>
+                  <p>{'{'} author: {post.postAuth} {'}'}</p>
+                </header>
+                <section className="post_Image">
+                  <img src={post.postImg} />
+                </section>
+                <section>
+                  <h3>{post.postTitle}</h3>
+                  <p>{post.postDesc}</p>
+                  <p><a href={post.postUrl}>karlchvojka.com</a></p>
+                </section>
+                <section className="socialCounters">
+                  <div className="socialComments">
+                    <img src="https://img.icons8.com/metro/26/000000/comments.png" alt="Speech Bubble Icon"/>
+                    <p>{post.postComments}</p>
+                  </div>
+                  <div className="socialRetweets">
+                    <img src="https://img.icons8.com/material-sharp/24/000000/retweet.png" alt="Retweet Icon"/>
+                    <p>{post.postRetweets}</p>
+                  </div>
+                  <div className="socialLikes">
+                    <img src="https://img.icons8.com/windows/32/000000/like.png" alt="Likes Icon"/>
+                    <p>{post.postLikes}</p>
+                  </div>
+                  <div className="socialSendDM">
+                    <img src="https://img.icons8.com/windows/32/000000/secured-letter--v1.png" alt="Letter Icon"/>
+                  </div>
+                </section>
+              </article>
+            </section>
+          </article>
+        </main>
+      </div>
     :
-    <h2>Loading...</h2>
+      <h2>Loading...</h2>
   );
 }
 
