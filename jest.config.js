1d909908909ee0dac6d172bc76ec322da36735e9
Karl Chvojka <karl.chvojka@protonmail.com>
2020-10-03 23:04:48 -0400

    Finished PostContent prop tests

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,20 @@
 module.exports = {
+  verbose: true,
   "moduleNameMapper": {
     "\\.(css|less|scss|sass)$": "<rootDir>/CSSStub.js"
   },
+  "collectCoverage": true,
   "collectCoverageFrom": [
-    "**/*.{js,jsx,ts,tsx}",
-    "!<rootDir>/node_modules/"
+    "**/*.{js,jsx}"
   ],
   modulePathIgnorePatterns: [
     "<rootDir>/jest.config.js",
     "<rootDir>/webpack.config.js",
     "<rootDir>/coverage/",
-    "<rootDir>/public/"
+    "<rootDir>/public/",
+    "<rootDir>/src/setupTests.js",
+    "<rootDir>/src/serviceWorker.js",
+    "<rootDir>/node_modules"
   ],
-  "collectCoverage": true,
-  testEnvironment: 'node'
+  testEnvironment: 'jsdom',
 };
