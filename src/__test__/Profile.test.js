6795b37cd5d7e72e02f40f1fa9e197afd443e103
Karl Chvojka <karl.chvojka@protonmail.com>
2020-10-04 18:09:17 -0400

    Profile props test complete

--- src/__test__/Profile.test.js
+++ src/__test__/Profile.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Profile from '../Components/Molecules/Profile';
 
@@ -27,5 +27,45 @@ describe('<Profile /> rendering', () => {
 
   it("renders a span tag", () => {
     expect(wrapper.find('span')).toHaveLength(1);
-  })
+  });
+});
+
+describe("<ShareCard /> props tests", () => {
+
+  const post = {
+    "title": "Developing JS applications with ReactJS",
+    "excerpt": "ReactJS is an amazing framework.Grapple shrouds stern crack Jennys tea cup Nelsons folly coxswain Sink me reef rigging tender. Broadside run a shot across the bow jack splice the main brace black spot bucko hardtack driver hands capstan.",
+    "url": "https://karlchvojka.com/blog/post-134",
+    "domain": "https://karlchvojka.com",
+    "auth": "@KarlChvojka",
+    "date": "Oct 15",
+    "img": "/developer.jpeg",
+    "comments": 2,
+    "retweets": 23,
+    "likes": 345
+  };
+
+  const user = {
+    login: {
+      username: 'karl.chvojka'
+    }
+  }
+
+  const profileWrapper = mount(
+    <Profile
+      author={post.auth}
+      authUrl={post.domain}
+      postDate={post.date}
+      title={post.title}
+      username={user.login.username}
+    />
+  );
+
+  it("Accepts the props", () => {
+    expect(profileWrapper.props().author).toEqual(post.auth);
+    expect(profileWrapper.props().authUrl).toEqual(post.domain);
+    expect(profileWrapper.props().postDate).toEqual(post.date);
+    expect(profileWrapper.props().title).toEqual(post.title);
+    expect(profileWrapper.props().username).toEqual(user.login.username);
+  });
 })
