6596165e8a90ad728b69fbfbd3cf7cac994d4346
Karl Chvojka <karl.chvojka@protonmail.com>
2020-10-03 22:30:52 -0400

    Finished Image tests

--- src/__test__/Image.test.js
+++ src/__test__/Image.test.js
@@ -1,13 +1,29 @@
+/**
+ * @jest-environment jsdom
+*/
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Image from '../Components/Atoms/Image';
 
 Enzyme.configure({ adapter: new Adapter() })
 
-
-describe('<Image/> rendering', () => {
+describe("<Image/> rendering", () => {
   it("renders Image component without crashing", () => {
     shallow(<Image />);
+  });
+});
+
+describe("<Image/> props tests", () => {
+  const image = {
+    alt: "An alt tag",
+    url: "http://www.google.com"
+  };
+
+  const imageWrapper = mount(<Image alt={image.alt} url={image.url}/>);
+
+  it("Accepts the props", () => {
+    expect(imageWrapper.props().alt).toEqual(image.alt);
+    expect(imageWrapper.props().url).toEqual(image.url);
   });
 })
