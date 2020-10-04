6271d0acac20f4993112fc5e75375b5d30aa063a
Karl Chvojka <karl.chvojka@protonmail.com>
2020-10-04 17:47:43 -0400

    SocialShare props testing

--- src/__test__/SocialShare.test.js
+++ src/__test__/SocialShare.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import SocialShare from '../Components/Organisms/SocialShare';
 
@@ -22,4 +22,33 @@ describe('<SocialShare /> rendering', () => {
     expect(wrapper.find('AiOutlineRetweet')).toHaveLength(1);
   });
 
+});
+
+describe("<SocialShare /> props tests", () => {
+  const post = {
+    "title": "Developing JS applications with ReactJS",
+    "excerpt": "ReactJS is an amazing framework.Grapple shrouds stern crack Jennys tea cup Nelsons folly coxswain Sink me reef rigging tender. Broadside run a shot across the bow jack splice the main brace black spot bucko hardtack driver hands capstan.",
+    "url": "https://karlchvojka.com/blog/post-134",
+    "domain": "https://karlchvojka.com",
+    "auth": "@KarlChvojka",
+    "date": "Oct 15",
+    "img": "/developer.jpeg",
+    "comments": 2,
+    "retweets": 23,
+    "likes": 345
+  };
+
+  const socialShareWrapper = mount(
+    <SocialShare
+      comments={post.comments}
+      likes={post.likes}
+      retweets={post.retweets}
+    />
+  );
+
+  it("Accepts the props", () => {
+    expect(socialShareWrapper.props().comments).toEqual(post.comments);
+    expect(socialShareWrapper.props().likes).toEqual(post.likes);
+    expect(socialShareWrapper.props().retweets).toEqual(post.retweets);
+  });
 })
