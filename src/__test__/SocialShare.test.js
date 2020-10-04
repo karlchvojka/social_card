8da7fbcca9886a39e1913e5d2b1a4b4930c17f48
Karl Chvojka <karl.chvojka@protonmail.com>
2020-10-03 22:06:45 -0400

    SocialShare Render Tests Complete.

--- src/__test__/SocialShare.test.js
+++ src/__test__/SocialShare.test.js
@@ -5,9 +5,21 @@ import SocialShare from '../Components/Organisms/SocialShare';
 
 Enzyme.configure({ adapter: new Adapter() })
 
-
+const wrapper = shallow(<SocialShare />);
+console.log(wrapper.debug());
 describe('<SocialShare /> rendering', () => {
   it("renders App component without crashing", () => {
-    shallow(<SocialShare />);
+    expect(wrapper.find('.socialCounters')).toHaveLength(1);
   });
+
+  it("renders three SocialCounter components", () => {
+    expect(wrapper.find('SocialCounter')).toHaveLength(3);
+  });
+
+  it("renders the socialSendDM section", () => {
+    expect(wrapper.find('.socialSendDM')).toHaveLength(1);
+    expect(wrapper.find('.counterIcon')).toHaveLength(1);
+    expect(wrapper.find('AiOutlineRetweet')).toHaveLength(1);
+  });
+
 })
